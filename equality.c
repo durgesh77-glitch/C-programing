67614d867ca94eded145acd25e7ce7e783c376dc
durgesh77-glitchgit  config --global user.email gdurgesh496@gmail.com <gdurgesh496@gmail.com>
2026-03-20 21:16:59 +0530

    Check equality

---
+++ equality.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{int a,b;
+printf ("Enter first no.:");
+    scanf ("%d",&a);
+    printf ("Enter second no.:");
+    scanf ("%d",&b);
+   /* if (!(a < b) && !(a > b )){
+        printf ("Both no. are equal\n");
+    }*/
+    else {
+        printf (" No. are not equal");
+    }
+    return 0;
+}
